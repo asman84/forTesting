024ff7e25b24b61266d5742d2e9aecfd4baeed8c
asman84 <odildjamalov@gmail.com>
2018-01-19 00:41:26 +0500

    vue filter va sort

--- js/test.js
+++ js/test.js
@@ -1,4 +1,23 @@
-console.log('JS is working');
-$("button").click(function(event) {
-    $('h1').toggleClass('text-red');
+var app1 = new Vue({
+    el: '#app-1',
+    data: {
+        show: true,
+        server: [{
+                name: 'Odil',
+                age: 23
+            },
+            {
+                name: 'Oybek',
+                age: 20
+            },
+            {
+                name: 'Zafar',
+                age: 12
+            },
+            {
+                name: 'Nassi',
+                age: 13
+            }
+        ]
+    }
 });
