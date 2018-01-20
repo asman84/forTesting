2c3ddbee81d8ad45b232e70711d63530ca077d77
asman84 <odildjamalov@gmail.com>
2018-01-20 22:32:39 +0500

    updated

--- js/test.js
+++ js/test.js
@@ -4,20 +4,81 @@ var app1 = new Vue({
         show: true,
         server: [{
                 name: 'Odil',
-                age: 23
+                birthday: '1984-11-11'
             },
             {
                 name: 'Oybek',
-                age: 20
+                birthday: '1987-08-09'
             },
             {
                 name: 'Zafar',
-                age: 12
+                birthday: '1995-07-11'
             },
             {
                 name: 'Nassi',
-                age: 13
+                birthday: '1994-07-12'
             }
         ]
+    },
+    filters: {
+        makeBig: function(value) {
+            value = value.toString();
+            let lastBigValue = value.slice(0, value.length - 1) + value.slice(value.length - 1).toUpperCase();
+            return lastBigValue;
+        },
+        ageIdentify: function(date) {
+            return moment().diff(moment(date), 'years');
+        }
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        filters: [
+            'all',
+            'today',
+            'tomorrow',
+        ],
+        active_filter: null,
+        todos: [{
+                text: 'call mom',
+                date: '2018-01-20',
+            },
+            {
+                text: 'buy apple',
+                date: '2018-01-19',
+            },
+            {
+                text: 'read news',
+                date: '2018-01-21',
+            },
+        ],
+    },
+    methods: {
+        chooseCategory: function(item) {
+            this.active_filter = item;
+        }
+    },
+    computed: {
+        sortedTodos: function(this.todos) {
+            var sortedArr = [];
+            if (this.active_filter === 'today') {
+                this.todos.forEach(function(item) {
+                    if (moment().startOf('day').diff(moment(item.date), 'days') === 0) {
+                        sortedArr.push(item);
+                    }
+                });
+            } else if (this.active_filter === 'tomorrow') {
+                this.todos.forEach(function(item) {
+                    if (moment().diff(moment(item.date.), 'days') === 1) {
+                        sortedArr.push(item);
+                    }
+                });
+            } else {
+                sortedArr.push(item);
+            }
+            return sortedArr;
+        }
     }
 });
